62898084b199f1957c3f6a984196cf58fc1a0c76
Francesc Obrador <obrador.francesc@gmail.com>
2024-08-07 20:03:01 +0200

    Created Resources
    Created resources for authors, books, categories, genres and publishers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { AuthorsModule } from './authors/authors.module';
+import { GenresModule } from './genres/genres.module';
+import { PublishersModule } from './publishers/publishers.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [BooksModule, AuthorsModule, GenresModule, PublishersModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
